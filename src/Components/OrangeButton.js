bc0e52cd1b82cc7fe3d6a0970d0941dfe4a8c95a
ADITIAGARWAL2801 <aditi28agarwal@gmail.com>
2024-07-05 16:00:37 +0530

    new changes

--- src/Components/OrangeButton.js
+++ src/Components/OrangeButton.js
@@ -5,9 +5,9 @@ import './OrangeButton.css'; // Import CSS for styling
 
 function OrangeButton() {
   return (
-    <div className="orange-button">
-      <p className="button-text">Real Time and Historical <br/> World Weather and Data API</p>
-      <p className="text2">Retrieve instant, accurate weather information for <br/> any location in the world in lightweight json format </p>
+    <div className="orange-button weather-intro">
+      <h1>Real Time and Historical <br/> World Weather and Data API</h1>
+      <p>Retrieve instant, accurate weather information for <br/> any location in the world in lightweight json format </p>
       <button className="rectangular-button">Get API</button>
     </div>
   );
